1196c284f23765df84251140554717af4c2513e1
decisionenabler-sk <surbhi.katy@gmail.com>
2025-05-15 14:44:58 -0700

    Adding the solution for monthly_pct_diff

--- monthly_pct_diff.sql
+++ monthly_pct_diff.sql
@@ -11,4 +11,21 @@ purchase_id: bigint
 
 */
 
-SELECT * FROM sf_transactions
+WITH monthly_revenue AS (
+    SELECT 
+    DATE_FORMAT(created_at, '%Y-%m') AS revenue_year_month,
+    SUM(transaction_value) AS total_monthly_revenue
+FROM sf_transactions
+GROUP BY 1
+)
+SELECT 
+revenue_year_month,
+total_monthly_revenue,
+ROUND(
+        ((total_monthly_revenue - LAG(total_monthly_revenue) OVER (ORDER BY revenue_year_month)) * 100.00/ 
+        LAG(total_monthly_revenue) OVER (ORDER BY revenue_year_month) ), 
+        2
+    ) AS revenue_pct_change
+FROM monthly_revenue
+ORDER BY 1
+;
